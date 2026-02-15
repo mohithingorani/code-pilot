ce583833a4a015d01d9a21c097bef6bed955c0f3
Mohit Hingorani <mohithingorani2003@users.noreply.github.com>
2026-02-15 20:14:50 +0530

    made changes

---
+++ frontend/app/data.ts
@@ -0,0 +1,18 @@
+export const EDITOR_ICONS = [
+    {
+        icon_name:"files",
+        href:"/icons/file-icon.svg"
+    },
+    {
+        icon_name:"search",
+        href:"/icons/search-icon.svg"
+    },
+     {
+        icon_name:"settings",
+        href:"/icons/settings-icon.svg"
+    },
+     {
+        icon_name:"version",
+        href:"/icons/version-icon.svg"
+    }
+]
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,6 +4,8 @@ import { useEffect, useRef, useState, } from "react";
 import type { editor as MonacoEditor } from "monaco-editor";
 import XTerminal from "./components/Terminal";
 import { useSocket } from "./hooks/websocket";
+import Image from "next/image";
+import { EDITOR_ICONS } from "./data";
 
 const Home = () => {
 
@@ -32,23 +34,29 @@ const Home = () => {
     return () => clearTimeout(sendCode);
   },[currentVal,connected])
 
-  
+  const handleEditorWillMount = (monaco:typeof import("monaco-editor")) => {
+    monaco.editor.defineTheme('my-custom-theme', {
+      base: 'vs-dark', // can also be 'vs' (light) or 'hc-black' (high contrast)
+      inherit: true, // set to false to completely replace the built-in rules
+      rules: [
+        { token: '', foreground: 'FFFFFF', background: '222222' },
+        { token: 'keyword', foreground: '569CD6' },
+        { token: 'identifier', foreground: '9CDCFE' },
+        { token: 'string', foreground: 'CE9178' },
+        { token: 'number', foreground: 'B5CEA8' },  
+        { token: 'comment', foreground: '6A9955', fontStyle: 'italic' },
+      ],
+      colors: {
+        'editor.background': '#222222', // Set the background color here
+      },
+    });
+  };
 
 
   useEffect(()=>{
     if(!socket) return;
     console.log("Setting up socket listeners");
-    // socket.onmessage = (event) => {
-    //   console.log("Received message:", event.data);
-    //   if(typeof event.data === "string") {
-    //     const parsed = JSON.parse(event.data);
-    //     console.log("Received message:", parsed);
-    //     if(parsed.type === "files") {
-    //       console.log("Received files:", parsed.data);
-    //       setFiles(parsed.data);
-    //     }
-    //   }
-    // };
+
 
     
 
@@ -74,31 +82,79 @@ const Home = () => {
 
 
 
- 
-  return (
-    <div>
-    {(!socket || files.length === 0 ) && <div className="w-full z-50 h-full bg-white">Loading...</div>}
-    <div className="h-full w-full">
+//  if(!socket){
+//   return <div className="w-full z-50 h-full bg-white">Loading...</div>
+//  }
+
+//   return (
+//     <div>
+//     <div className="h-full w-full">
       
-    <div className="flex h-full justify-start  w-full" >
-    <div>
-      {files &&files.map((file)=><div key={file.name}>
-        <h3>{file.name}</h3>
-      </div>)}
-    </div>    <div className="h-full">
-      {/* <div onKeyDown={(e)=>{ e.key=="Enter"&& socket?.send(currentVal)}}> */}
-    <div>
-    {currentVal!=null && <Editor onChange={handleEditorDidChange}  height={"70vh"} className="min-w-xl h-full" value={currentVal} defaultLanguage={"python"}  theme="vs-dark" onMount={handleEditorDidMount}/>}
-    </div>
-    </div>
-    </div>
-    <div className="z-10">
-      {<XTerminal socket={socket}/>}
-    </div>
+//     <div className="flex h-full justify-start  w-full" >
+//     <div>
+//       {files &&files.map((file)=><div key={file.name}>
+//         <h3>{file.name}</h3>
+//       </div>)}
+//     </div>    <div className="h-full">
+//       {/* <div onKeyDown={(e)=>{ e.key=="Enter"&& socket?.send(currentVal)}}> */}
+//     <div>
+//     {currentVal!=null && <Editor onChange={handleEditorDidChange}  height={"70vh"} className="min-w-xl h-full" value={currentVal} defaultLanguage={"python"}  theme="vs-dark" onMount={handleEditorDidMount}/>}
+//     </div>
+//     </div>
+//     </div>
+  
+
+//       </div>
+//       </div>
+//   );
+// };
+
+return (
+  <div className="w-full h-full flex justify-center items-center py-12">
+    <div className=" w-5xl h-full rounded-lg  flex flex-start overflow-hidden relative">
+      <div className="w-xs text-white bg-black/50 py-2  backdrop-filter backdrop-blur-md ">
+      <div className="flex justify-center items-center font-medium text-lg">Editor</div>
+      <div><EditorOptions/></div>
+      </div>
+      <div className="w-full max-h-full relative ">
+        {headingTabs(files)}
+        <div className=" w-full">
+          <Editor onChange={handleEditorDidChange} beforeMount={handleEditorWillMount} height={"83vh"}   className="w-full " value={currentVal || ""} defaultLanguage={"python"}  theme="my-custom-theme" onMount={handleEditorDidMount}/>
+        </div>
+         <div className="absolute z-100 bottom-0 max-h-64 w-full bg-green-300">
+        <XTerminal socket={socket}/>
 
       </div>
       </div>
-  );
-};
+     
+    </div>
+  </div>
+)
+}
+
+  {/* <div className="z-10">
+      {<XTerminal socket={socket}/>}
+    </div> */}
 
 export default Home;
+
+
+function headingTabs(files:{name:string,content:string}[]){
+  return (
+    <div className="bg-[#191919] text-white text-sm py-2 h-fit">
+      {files.map((file)=><span key={file.name} className="px-4 cursor-pointer hover:bg-white/10 py-4">{file.name}</span>)}
+    </div>
+  )
+}
+
+function EditorOptions(){
+  return (
+
+      <div className="flex gap-4 w-full justify-center py-4">
+        {EDITOR_ICONS.map((icon)=>
+        <button key={icon.icon_name}  className="p-2 hover:bg-white/10 rounded">
+        <Image src={icon.href} alt={icon.icon_name} width={25} height={25}/>
+        </button>)}
+      </div>
+  )
+}
